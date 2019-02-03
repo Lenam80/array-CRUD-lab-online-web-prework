c02d468ae02d5b3fa71ecfb55dff32f28392ba43
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-03 18:40:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,15 +1,3 @@
-def instaniate_new_array
-	Array.new
-end
-
-def array_with_two_elements
-	[1, 2]
-end
-
-def first_element(array)
-	array[0]
-end
-
-def third_element(array)
-	array[2]
+def create_an_empty_array
+  [1,2,2,3,3learn]
 end
