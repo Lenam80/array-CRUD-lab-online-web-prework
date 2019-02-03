6c2e554967d115c4c67b3413590ec7d464982502
Angelina Montanez <angelinamontanez80@gmail.com>
2019-02-03 18:39:34 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,15 @@
-def create_an_empty_array
-  
+def instaniate_new_array
+	Array.new
 end
 
-def create_an_array
-  
+def array_with_two_elements
+	[1, 2]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def first_element(array)
+	array[0]
 end
 
-def add_element_to_start_of_array(array, element)
-  
-end
-
-def remove_element_from_end_of_array(array)
-  
-end
-
-def remove_element_from_start_of_array(array)
-  
-end
-
-def retrieve_element_from_index(array, index_number)
-  
-end
-
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
+def third_element(array)
+	array[2]
 end
